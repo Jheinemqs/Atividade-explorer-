16d3a1f87a37fb378767a70a91ac9f986dfce987
Jheinemqs <jhei_vitoria1@hotmail.com>
2022-11-03 20:40:11 -0400

    3 aula

---
+++ main.js
@@ -0,0 +1,43 @@
+function createGame(player1, hour, player2) {
+  return ` 
+  <li>
+   <img src="./assets/icon-${player1}.svg" alt=" Bandeira do ${player1}" />
+   <strong>${hour}</strong>
+   <img src="./assets/icon-${player2}.svg" alt=" Bandeira da ${player2}" />
+ </li>
+ `
+}
+
+function createCard(date, day, games) {
+  return `
+      <div class="card">
+          <h2> ${date} <span> ${day} </span> </h2>
+          <ul>
+           ${games}
+          </ul>
+        </div>
+  `
+}
+
+document.querySelector("#app").innerHTML = `
+      <header>
+        <img src="./assets/logo.svg" alt="Logo da NLW" />
+      </header>
+      <main id="cards">
+        ${createCard(
+          "24/11",
+          "Quinta",
+          createGame("brazil", "16:00", "Serbia")
+        )} 
+        ${createCard(
+          "28/11",
+          "Segunda",
+          createGame("brazil", "13:00", "switzerland")
+        )} 
+        ${createCard(
+          "02/12",
+          "Sexta",
+          createGame("brazil", "16:00", "Cameroon")
+        )}
+      </main>
+`
